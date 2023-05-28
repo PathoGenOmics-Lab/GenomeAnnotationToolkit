58425fac59879e98586d12620deb5fd1f7856811
Paula Ruiz-Rodriguez <50167687+Paururo@users.noreply.github.com>
2023-05-28 12:24:30 +0200

    Update variables gbk2tsv.py

--- gbk2tsv.py
+++ gbk2tsv.py
@@ -9,8 +9,8 @@ def parse_args():
     parser.add_argument("-g", "--gbk", nargs = "+", type = str, required = True, dest = "gbks", default = "", help = "Specify GenBank files as input")
     parser.add_argument("-o", "--outdir", type = str, required = False, dest = "outdir", default = ".", help = "Define the directory for output files")
     parser.add_argument("-f", "--features", type = str, required = False, dest = "features", default = "CDS,tRNA,rRNA", help = "Features to include, separated by commas")
-    parser.add_argument("-n", "--nucl_seq", action = "store_true", required = False, dest = "nucl_seq", help = "Enable this to include nucleotide sequences of features")
-    parser.add_argument("-p", "--prot_seq", action = "store_true", required = False, dest = "prot_seq", help = "Enable this to include protein sequences of CDS")
+    parser.add_argument("-n", "--nucleotides", action = "store_true", required = False, dest = "nucleotides", help = "Enable this to include nucleotide sequences of features")
+    parser.add_argument("-p", "--protein", action = "store_true", required = False, dest = "protein", help = "Enable this to include protein sequences of CDS")
     return parser.parse_args()  # Returns an instance of the class ArgumentParser
 
 def get_input_filenames(gbks):
@@ -21,10 +21,10 @@ def get_input_filenames(gbks):
 
 def prepare_header(args):
     header = ["Contig", "Locus", "Feature", "Start", "End", "Strand", "Pseudo", "Product", "Note"]
-    if args.nucl_seq:
-        header.append("Nucl_seq")
-    if args.prot_seq:
-        header.append("Prot_seq")
+    if args.nucleotides:
+        header.append("Nucleotide_Seq")
+    if args.protein:
+        header.append("Protein_Seq")
     return header
 
 def process_records(records, features, args):
@@ -44,9 +44,9 @@ def process_feature(feature, contig, args):
     note = feature.qualifiers.get("note", ["unknown"])[0]
 
     line = [contig, locus_tag, feature.type, str(feature.location.nofuzzy_start + 1), str(feature.location.nofuzzy_end), strand, is_pseudo, product, note]
-    if args.nucl_seq:
+    if args.nucleotides:
         line.append(str(feature.extract(record.seq)))
-    if feature.type == "CDS" and args.prot_seq:
+    if feature.type == "CDS" and args.protein:
         line.append(feature.qualifiers.get("translation", ["unknown"])[0])
     return line
 
