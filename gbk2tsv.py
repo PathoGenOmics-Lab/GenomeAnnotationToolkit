ef4be2e91453614359b77ad30a64f862d4fb480d
Paula Ruiz-Rodriguez <50167687+Paururo@users.noreply.github.com>
2023-05-28 12:08:01 +0200

    add input options gbk2tsv.py

--- gbk2tsv.py
+++ gbk2tsv.py
@@ -4,7 +4,17 @@ import pathlib
 from Bio import SeqIO, SeqFeature
 from argparse import ArgumentParser
 
+def parse_args():
+    parser = ArgumentParser(description = "Convert GenBank files to tab-delimited text files")
+    parser.add_argument("-g", "--gbk", nargs = "+", type = str, required = True, dest = "gbks", default = "", help = "Input GenBank files")
+    parser.add_argument("-o", "--outdir", type = str, required = False, dest = "outdir", default = ".", help = "Output directory")
+    parser.add_argument("-f", "--features", type = str, required = False, dest = "features", default = "CDS,tRNA,rRNA", help = "Comma-separated features to store")
+    parser.add_argument("-n", "--nucl_seq", action = "store_true", required = False, dest = "nucl_seq", help = "Turn on this option to print nucleotide sequences of features")
+    parser.add_argument("-p", "--prot_seq", action = "store_true", required = False, dest = "prot_seq", help = "Turn on this option to print protein sequences of CDS")
+    
+    return parser.parse_args()
+
 def main():
-  
+    args = parse_args()  
 if __name__ == "__main__":
     main()
