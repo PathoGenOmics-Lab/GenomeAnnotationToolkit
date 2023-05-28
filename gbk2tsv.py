1d0ced3edd30475e2bb1eda564b10c1d0c7c6fcf
Paula Ruiz-Rodriguez <50167687+Paururo@users.noreply.github.com>
2023-05-28 12:16:38 +0200

    Update argparse gbk2tsv.py

--- gbk2tsv.py
+++ gbk2tsv.py
@@ -5,13 +5,13 @@ from Bio import SeqIO, SeqFeature
 from argparse import ArgumentParser
 
 def parse_args():
-    parser = ArgumentParser(description = "Convert GenBank files to tab-delimited text files")
-    parser.add_argument("-g", "--gbk", nargs = "+", type = str, required = True, dest = "gbks", default = "", help = "Input GenBank files")
-    parser.add_argument("-o", "--outdir", type = str, required = False, dest = "outdir", default = ".", help = "Output directory")
-    parser.add_argument("-f", "--features", type = str, required = False, dest = "features", default = "CDS,tRNA,rRNA", help = "Comma-separated features to store")
-    parser.add_argument("-n", "--nucl_seq", action = "store_true", required = False, dest = "nucl_seq", help = "Turn on this option to print nucleotide sequences of features")
-    parser.add_argument("-p", "--prot_seq", action = "store_true", required = False, dest = "prot_seq", help = "Turn on this option to print protein sequences of CDS")    
-    return parser.parse_args()
+    parser = ArgumentParser(description = "Transforms GenBank files into tab-separated value files")
+    parser.add_argument("-g", "--gbk", nargs = "+", type = str, required = True, dest = "gbks", default = "", help = "Specify GenBank files as input")
+    parser.add_argument("-o", "--outdir", type = str, required = False, dest = "outdir", default = ".", help = "Define the directory for output files")
+    parser.add_argument("-f", "--features", type = str, required = False, dest = "features", default = "CDS,tRNA,rRNA", help = "Features to include, separated by commas")
+    parser.add_argument("-n", "--nucl_seq", action = "store_true", required = False, dest = "nucl_seq", help = "Enable this to include nucleotide sequences of features")
+    parser.add_argument("-p", "--prot_seq", action = "store_true", required = False, dest = "prot_seq", help = "Enable this to include protein sequences of CDS")
+    return parser.parse_args()  # Returns an instance of the class ArgumentParser
 
 def get_input_filenames(gbks):
     gbk_list = list(gbks)
