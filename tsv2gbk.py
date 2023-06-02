b87fc47d388384043bc9fd390e8de1c0f3ea8262
Paula Ruiz <paururo@gmail.com>
2023-06-02 10:04:00 +0200

    add modules to use

--- tsv2gbk.py
+++ tsv2gbk.py
@@ -1,2 +1,11 @@
 #! /usr/bin/python3
 # -*- coding: utf-8 -*-
+
+import sys
+import csv
+import argparse
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio.SeqFeature import SeqFeature, FeatureLocation
+from Bio.Data import CodonTable
