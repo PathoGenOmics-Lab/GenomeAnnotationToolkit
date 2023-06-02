93f7a632fce53ce2fc466af2f1c06f8eb830ee84
Paula Ruiz <paururo@gmail.com>
2023-06-02 10:16:14 +0200

    minor chg

--- tsv2gbk.py
+++ tsv2gbk.py
@@ -1,14 +1,12 @@
 #! /usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import sys
 import csv
 import argparse
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from Bio.SeqFeature import SeqFeature, FeatureLocation
-
 
 def tsv_to_gbk(tsv_file, fasta_file, gbk_file, molecule_type='DNA'):
     with open(tsv_file, 'r') as tsv:
