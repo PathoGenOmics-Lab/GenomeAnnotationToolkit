55ff02e85e4714a517f2ea5bf75ab0ad22fefad7
Paula Ruiz <paururo@gmail.com>
2023-06-02 10:08:54 +0200

    Read the fasta file and create a SeqRecord

--- tsv2gbk.py
+++ tsv2gbk.py
@@ -13,6 +13,13 @@ from Bio.Data import CodonTable
 def tsv_to_gbk(tsv_file, fasta_file, gbk_file, molecule_type='DNA'):
     with open(tsv_file, 'r') as tsv:
         tsv_reader = csv.reader(tsv, delimiter='\t')
+        
+        for record in SeqIO.parse(fasta_file, 'fasta'):
+            fasta_name = fasta_file.split('/')[-1].split('.')[0]
+            seq_record = SeqRecord(Seq(str(record.seq)), id=fasta_name, description=record.description)
+
+        seq_record.annotations['molecule_type'] = molecule_type
+
 
 
 def parse_args():
